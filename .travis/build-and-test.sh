01ccb972dbe194d0ec0299cd8c75d50f0bcb592b
Jacob Bouffard <jbouffard@azavea.com>
2018-10-03 11:41:26 -0400

    Fixed how Travis runs the unit tests
    Fixed typo in Travis script
    
    Changed how Travis runs the tests

--- .travis/build-and-test.sh
+++ .travis/build-and-test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-./sbt -J-Xmx2G "++$TRAVIS_SCALA_VERSION" test  || { exit 1; }
+./sbt -J-Xmx2G "++$TRAVIS_SCALA_VERSION" \
+  "project gdal" test \
+  "project gdal-etl" test || { exit 1; }
