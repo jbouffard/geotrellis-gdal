e8c049cec46b0426bfc45d70209c5ac80d887dca
Grigory Pomadchin <gr.pomadchin@gmail.com>
2019-01-09 15:30:37 +0300

    Update GDAL up to 2.4.0

--- .travis/build-and-test-docker.sh
+++ .travis/build-and-test-docker.sh
@@ -6,4 +6,4 @@ docker run -it --net=host \
   -v $TRAVIS_BUILD_DIR:/geotrellis-gdal \
   -e TRAVIS_SCALA_VERSION=$TRAVIS_SCALA_VERSION \
   -e TRAVIS_COMMIT=$TRAVIS_COMMIT \
-  -e TRAVIS_JDK_VERSION=$TRAVIS_JDK_VERSION daunnc/openjdk-gdal:2.3.2 /bin/bash -c "cd /geotrellis-gdal; .travis/build-and-test.sh"
+  -e TRAVIS_JDK_VERSION=$TRAVIS_JDK_VERSION daunnc/openjdk-gdal:2.4.0 /bin/bash -c "cd /geotrellis-gdal; .travis/build-and-test.sh"
